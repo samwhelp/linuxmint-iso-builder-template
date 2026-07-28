f3d4cbb149189e2b83dfa8e6a9cef45c5854ea70
samwhelp <samwhelp@users.noreply.github.com>
2026-07-28 18:21:04 +0800

    fix

--- template/engine/libs/domain/worker/model.sh
+++ template/engine/libs/domain/worker/model.sh
@@ -45,15 +45,23 @@ function core_var_dump () {
 
 }
 
+function raw_building_var_dump () {
+
+	echo "APT_SOURCE=${APT_SOURCE}"
+	echo "TARGET_UBUNTU_VERSION=${TARGET_UBUNTU_VERSION}"
+	echo "TARGET_ARCH=${TARGET_ARCH}"
+	echo "TARGET_NAME=${TARGET_NAME}"
+	echo "TARGET_BUSINESS_NAME=${TARGET_BUSINESS_NAME}"
+	echo "TARGET_BUILD_VERSION=${TARGET_BUILD_VERSION}"
+	echo "PKG_SERVER=${PKG_SERVER}"
+
+}
+
 function core_building_var_dump () {
 
 	print_info "Dump building variables"
 
-	echo "TARGET_UBUNTU_VERSION=${TARGET_UBUNTU_VERSION}"
-	echo "APT_SOURCE=${APT_SOURCE}"
-	echo "TARGET_NAME=${TARGET_NAME}"
-	echo "TARGET_BUSINESS_NAME=${TARGET_BUSINESS_NAME}"
-	echo "TARGET_BUILD_VERSION=${TARGET_BUILD_VERSION}"
+	raw_building_var_dump
 
 	judge "Dump building variables"
 
